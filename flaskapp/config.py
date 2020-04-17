ed768af721e2ec2337c64fa59abc664bb53fbd7a
kkornel <kornel.krzeslak@student.put.poznan.pl>
2020-04-17 16:49:19 +0200

    Set up config

--- flaskapp/config.py
+++ flaskapp/config.py
@@ -9,14 +9,9 @@ import os
 
 class Config:
     DEBUG = False
-    # This should also be enviroment variable.
-    # SECRET_KEY = '046e22b94eeaa82d1a6d721f0da941ec'
-    SECRET_KEY = os.environ.get('MT_FLASK_APP_SECRET_KEY')
-    # This should also be enviroment variable.
-    # SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
-    # SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:postgres@localhost/mt_flask_app'
+    SECRET_KEY = os.environ.get('FLASK_SOCIAL_APP_SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get(
-        'MT_FLASK_SQLALCHEMY_DATABASE_URI')
+        'FLASK_SOCIAL_APP_SQLALCHEMY_DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOAD_FOLDER_POSTS_IMGS = 'static\posts_imgs'
     UPLOAD_FOLDER_PROFILES_IMGS = 'static\profiles_imgs',
@@ -29,9 +24,9 @@ class Config:
     RECAPTCHA_PUBLIC_KEY = os.environ.get('RECAPTCHA_PUBLIC_KEY')
     RECAPTCHA_PRIVATE_KEY = os.environ.get('RECAPTCHA_PRIVATE_KEY')
     RECAPTCHA_DATA_ATTRS = {'theme': 'dark'}
-    # To environ
-    SECURITY_RESET_PASSWORD_SALT = 'cbcb788d1334901da4b846c6adce4f0f'
-    SECURITY_VERIFY_EMAIL_SALT = '75df285855af40a28eea4760bae29f58'
+    SECURITY_RESET_PASSWORD_SALT = os.environ.get(
+        'SECURITY_RESET_PASSWORD_SALT')
+    SECURITY_VERIFY_EMAIL_SALT = os.environ.get('SECURITY_VERIFY_EMAIL_SALT')
 
 
 class DevelopmentConfig(Config):
